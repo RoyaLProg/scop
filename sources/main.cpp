e038affca8ed75eae1bb893a05d0cd74dafb3207
Camille CAMBIUM <royalfr@protonmail.com>
2024-02-07 16:34:47 +0100

    FORMAT : main file

--- sources/main.cpp
+++ sources/main.cpp
@@ -5,12 +5,11 @@
 int main() {
 	Screen screen;
 
-	if ( screen.getFailed() )
-	{
+	if ( screen.getFailed() ) {
 		return 1;
 	}
 
-	std::vector<vec3> & points = screen.getPoints();
+	std::vector<vec3>& points = screen.getPoints();
 
 	points.emplace_back( 100, 100, 100 );
 	points.emplace_back( 200, 100, 100 );
@@ -22,10 +21,18 @@ int main() {
 	points.emplace_back( 200, 200, 200 );
 	points.emplace_back( 100, 200, 200 );
 
-	screen.line( Connection(0, 4) );
-	screen.line( Connection(1, 5) );
-	screen.line( Connection(2, 6) );
-	screen.line( Connection(3, 7) );
+	screen.line( Connection( 0, 4 ) );
+	screen.line( Connection( 1, 5 ) );
+	screen.line( Connection( 2, 6 ) );
+	screen.line( Connection( 3, 7 ) );
+	screen.line( Connection( 0, 1 ) );
+	screen.line( Connection( 1, 2 ) );
+	screen.line( Connection( 2, 3 ) );
+	screen.line( Connection( 3, 0 ) );
+	screen.line( Connection( 4, 5 ) );
+	screen.line( Connection( 5, 6 ) );
+	screen.line( Connection( 6, 7 ) );
+	screen.line( Connection( 7, 4 ) );
 
 	vec3 c( 0, 0, 0 );
 
@@ -45,7 +52,7 @@ int main() {
 			point.y -= c.y;
 			point.z -= c.z;
 
-			point.rotate( 0.05f, 0.05f, 0.05f );
+			point.rotate( 0.0f, 0.0f, 0.05f );
 
 			point.x += c.x;
 			point.y += c.y;
@@ -54,6 +61,7 @@ int main() {
 
 		screen.show();
 		screen.input();
+		SDL_Delay( 1000 / 60 );
 	}
 
 	return 0;
